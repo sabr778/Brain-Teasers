3e36c47b93f59d71b5f107109e84a4dc1e14f356
sabr778 <saberyu778@gmail.com>
2017-06-22 09:35:12 -0700

    No.19 simulating toss the dice

---
+++ 19/test.py
@@ -0,0 +1,13 @@
+import random
+NUM = 100000
+cnt = [1,0,0,0,0,0,0]
+for i in range(1,NUM+1):
+	sum = 0
+	a = 0
+	while sum < 100:
+		a = random.randint(1,6)
+		sum += a
+	cnt[a] += 1
+for i in range(1,7):
+	a = float(cnt[i])/NUM
+	print "%d \t %f" % (i, a)
